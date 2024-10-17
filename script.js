082656a0e819e0c4a6f0afcd32781a59cec9ec50
DaleB34 <dalebolido@gmail.com>
2024-10-17 12:59:43 -0700

    fetchCSHours error TypeError: Failed to fetch

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 
-document.getElementById("addBtn").addEventListener("click", addRow);
+const apiKey = "C021017D2A839D32E50154A4E556C847";
 
-function addRow() {   
-        const para = document.createElement("div");
-        para.innerText = "Hello Geeks";
-        document.body.appendChild(para);
-}
+
+// document.getElementById("addBtn").addEventListener("click", addRow);
+
+// function addRow() {   
+//         const para = document.createElement("div");
+//         para.innerText = fetchCSHours();
+//         document.body.appendChild(para);
+// }
+fetchCSHours();
 
 async function fetchCSHours(){
+    const steamid = "76561198394533709";
 
     try{
-        const response = await fetch("https://api.steampowered.com/IPlayerService/GetOwnedGames/v1");
-        if(!response.ok)
-        {
+        const response = await fetch("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" + apiKey + "&steamid=" + steamid + "&format=json");
+        if(!response.ok){
             throw new Error("could not fetch resource");
         }
 
